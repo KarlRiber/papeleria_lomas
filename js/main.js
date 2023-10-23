450d62a9b26868946a3dc0f0e17c6c03ee975736
KarlRiber <95459034+KarlRiber@users.noreply.github.com>
2023-10-23 11:19:39 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+const username = document.getElementById("username");
+const psword = document.getElementById("psword");
+const submitbutton = document.getElementById("submitbutton");
+const contrasena = "1234"
+
+submitbutton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const interContrasena = psword.value;
+  
+  if (interContrasena == contrasena)
+    location.href = "/html/interfaz_01.html"
+  else
+  alert("Contraseña invalida");
+})
+
+
+
+
